c97d03760df4eb416b3673077bca76fcb21b3fec
Maadeha shaikh <maadeham@gmail.com>
2024-08-15 21:50:03 +0500

    done

--- index.js
+++ index.js
@@ -15,12 +15,18 @@ app.get('/',function(req,res){
   })
 })
 
-app.get('/',function(req,res){ 
-  fs.readdir(`./files`,function(err,files){
-    // console.log(files); it is making an array and whatever the file is in the files folder it'll put it into that array 
-    res.render("index", {files:files});
+app.post('/create',function(req,res){ 
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details ,function(err){
+  res.redirect("/");
+  });
+  // console.log(req.body);   
   })
-})
+
+  // app.post('/create',function(req,res){ 
+  //   console.log(typeof req.body.title, req.body.title);
+  //   })
+
+
 
 app.listen(3000,function(){
   console.log("Its working")
