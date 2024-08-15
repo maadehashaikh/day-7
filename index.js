53c6412f774ca011ce3943c772de95e188fa577c
Maadeha shaikh <maadeham@gmail.com>
2024-08-15 20:35:28 +0500

    done

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get('/',function(req,res){
   })
 })
 
+app.get('/',function(req,res){ 
+  fs.readdir(`./files`,function(err,files){
+    // console.log(files); it is making an array and whatever the file is in the files folder it'll put it into that array 
+    res.render("index", {files:files});
+  })
+})
+
 app.listen(3000,function(){
   console.log("Its working")
 });
